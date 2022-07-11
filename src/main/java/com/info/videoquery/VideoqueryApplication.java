e65bf7bbf3b2ba30ef22e9149a9be2bbc8b55e03
zsa332 <zsa332@gmail.com>
2022-07-11 21:45:11 +0900

    first Commit

---
+++ src/main/java/com/info/videoquery/VideoqueryApplication.java
@@ -0,0 +1,13 @@
+package com.info.videoquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoqueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoqueryApplication.class, args);
+	}
+
+}
