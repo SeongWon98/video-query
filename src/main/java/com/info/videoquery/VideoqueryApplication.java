f931df00f1cf848f542174354d03fff7f077aa14
zsa332 <zsa332@gmail.com>
2022-07-19 13:56:08 +0900

    sql 서버 연결

--- src/main/java/com/info/videoquery/VideoqueryApplication.java
+++ src/main/java/com/info/videoquery/VideoqueryApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VideoqueryApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VideoqueryApplication.class, args);
 	}
